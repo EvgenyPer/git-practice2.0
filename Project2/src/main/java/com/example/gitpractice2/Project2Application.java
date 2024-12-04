d7b85fd1c504be60b518888c014d1b9e75748161
Evgeny Per <kunnka123.one123@gmail.com>
2024-12-04 19:36:58 +0300

    Добавлены тест и CI

---
+++ Project2/src/main/java/com/example/gitpractice2/Project2Application.java
@@ -0,0 +1,16 @@
+package com.example.gitpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2Application.class, args);
+
+		System.out.println("Hello World!");
+
+	}
+
+}
